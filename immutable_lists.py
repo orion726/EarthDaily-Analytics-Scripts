58d0e437bd72728451b2117a046c1a73a2c7bd91
orion726 <jw.orion26@gmail.com>
2021-12-16 11:05:19 -0800

    Add files via upload

---
+++ immutable_lists.py
@@ -0,0 +1,44 @@
+def lists(full, add, remove):
+	"""
+	This function takes in an original list of strings, adds a new list of strings,
+	and then removes elements from the third list of strings. Duplicates are removed
+	and then the new list is sorted by 2 keys: character count and then reverse 
+	alphabetical.
+
+	Input:
+		full - list of strings, any type
+		add - list of strings, any type
+		remove - list of strings, any type
+
+	Output:
+		new_list - new list, tuple
+
+	This function runs successfully in Python 3.6.8
+		
+	"""
+
+	# Make easily editable copies of the input
+	full = [x for x in full]
+	add = [x for x in add]
+	remove = [x for x in remove]
+
+	# Append the 'full' and 'add' lists
+	full.extend(add)
+
+	# Remove duplicates and elements in the 'remove' list
+	full = [x for i, x in enumerate(full) if x not in full[:i] and x not in remove]
+
+	# Sort the list by character count and reverse alphabetical
+	full = sorted(full, key=lambda x: (len(x), x), reverse = True)
+
+	# Convert to tuple
+	new_list = tuple(full)
+
+	return tuple(new_list)
+
+
+a = ("one", "two", "three", "zulu")
+b = ("one", "two", "five", "six")
+c = ("two", "five")
+
+print(lists(a,b,c))
